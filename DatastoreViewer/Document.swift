081a1bb1a95e9d6d53053941f72d2c17ec3912fe
Sam Deane <sam@elegantchaos.com>
2019-12-20 11:37:42 +0000

    Initial Commit

---
+++ DatastoreViewer/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  DatastoreViewer
+//
+//  Created by Developer on 20/12/2019.
+//  Copyright © 2019 Elegant Chaos. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ DatastoreViewerTests/DatastoreViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  DatastoreViewerTests.swift
+//  DatastoreViewerTests
+//
+//  Created by Developer on 20/12/2019.
+//  Copyright © 2019 Elegant Chaos. All rights reserved.
+//
+
+import XCTest
+@testable import DatastoreViewer
+
+class DatastoreViewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
