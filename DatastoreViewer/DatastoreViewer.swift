5a5974c21ae906d033732a39642edae44bdfe73c
Sam Deane <sam@elegantchaos.com>
2020-02-04 16:57:39 +0000

    Use standard delegates from AppExtensions/LoggerKit

---
+++ DatastoreViewer/DatastoreViewer.swift
@@ -0,0 +1,27 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 20/12/2019.
+//  All code (c) 2019 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import UIKit
+import ApplicationExtensions
+
+@UIApplicationMain class DatastoreViewer: BasicApplication {
+    override func open(file url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        // Reveal / import the document at the URL
+        guard let documentBrowserViewController = window?.rootViewController as? DocumentBrowserViewController else { return false }
+
+        documentBrowserViewController.revealDocument(at: url, importIfNeeded: true) { (revealedDocumentURL, error) in
+            if let error = error {
+                // Handle the error appropriately
+                print("Failed to reveal the document at URL \(url) with error: '\(error)'")
+                return
+            }
+            
+            // Present the Document View Controller for the revealed URL
+            documentBrowserViewController.presentDocument(at: revealedDocumentURL!)
+        }
+
+        return true
+    }
+}
---
+++ DatastoreViewer/DatastoreViewerScene.swift
@@ -0,0 +1,10 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Developer on 04/02/2020.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import ApplicationExtensions
+
+class DatastoreViewerScene: BasicScene {
+}
+
